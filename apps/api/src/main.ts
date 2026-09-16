5f60a2c6074b1cdb4b896fe4a4f4b84678d4862a
Edwinc1987 <edwincarbonell1987@gmail.com>
2026-09-16 14:31:49 -0500

    Fijar driver HTTP de Nest local

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,13 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter } from "@nestjs/platform-express";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 export const apiVersion = "0.1.0";
 
 async function bootstrap(){
-  const app=await NestFactory.create(AppModule);
+  const app=await NestFactory.create(AppModule,new ExpressAdapter());
   app.enableCors();
   const config=new DocumentBuilder().setTitle("TOMFIC API").setDescription("API multiempresa de inventarios y CRM").setVersion("1.0").addBearerAuth().build();
   SwaggerModule.setup("docs",app,SwaggerModule.createDocument(app,config));
