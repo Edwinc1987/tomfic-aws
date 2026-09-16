366a99e15e4994f8e6e005e32c450db6ae14bced
Edwinc1987 <edwincarbonell1987@gmail.com>
2026-09-16 17:12:17 -0500

    Cifrar almacenamiento RDS

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -54,6 +54,7 @@ export class InfraStack extends cdk.Stack {
         instanceType:ec2.InstanceType.of(ec2.InstanceClass.T4G,ec2.InstanceSize.MICRO),
         allocatedStorage:20,
         maxAllocatedStorage:100,
+        storageEncrypted:true,
         databaseName:"tomfic",
         credentials:rds.Credentials.fromGeneratedSecret("tomfic_admin"),
         publiclyAccessible:false,
