0cdd7761b44eb71594dd2678c0d6ac7f491cf323
Edwinc1987 <edwincarbonell1987@gmail.com>
2026-09-16 14:41:01 -0500

    Definir S3 y colas AWS

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,5 +1,7 @@
 import * as cdk from 'aws-cdk-lib/core';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -20,6 +22,25 @@ export class InfraStack extends cdk.Stack {
     const userPoolClient=userPool.addClient("TomficWebClient",{authFlows:{userPassword:true,userSrp:true}});
     new cdk.CfnOutput(this,"UserPoolId",{value:userPool.userPoolId});
     new cdk.CfnOutput(this,"UserPoolClientId",{value:userPoolClient.userPoolClientId});
+
+    const deadLetterQueue=new sqs.Queue(this,"ImportDeadLetterQueue",{
+      encryption:sqs.QueueEncryption.SQS_MANAGED,
+      retentionPeriod:cdk.Duration.days(14),
+    });
+    const importQueue=new sqs.Queue(this,"ImportQueue",{
+      encryption:sqs.QueueEncryption.SQS_MANAGED,
+      visibilityTimeout:cdk.Duration.minutes(5),
+      deadLetterQueue:{queue:deadLetterQueue,maxReceiveCount:3},
+    });
+    const storageBucket=new s3.Bucket(this,"TomficPrivateStorage",{
+      blockPublicAccess:s3.BlockPublicAccess.BLOCK_ALL,
+      encryption:s3.BucketEncryption.S3_MANAGED,
+      enforceSSL:true,
+      versioned:true,
+      removalPolicy:cdk.RemovalPolicy.RETAIN,
+    });
+    new cdk.CfnOutput(this,"ImportQueueUrl",{value:importQueue.queueUrl});
+    new cdk.CfnOutput(this,"StorageBucketName",{value:storageBucket.bucketName});
 
     // example resource
     // const queue = new sqs.Queue(this, 'InfraQueue', {
