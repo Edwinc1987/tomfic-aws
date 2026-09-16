674fabb2e514d607e1327d587e114b9065bad956
Edwinc1987 <edwincarbonell1987@gmail.com>
2026-09-16 12:36:19 -0500

    Documentar API con OpenAPI

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 
 export const apiVersion = "0.1.0";
@@ -7,6 +8,8 @@ export const apiVersion = "0.1.0";
 async function bootstrap(){
   const app=await NestFactory.create(AppModule);
   app.enableCors();
+  const config=new DocumentBuilder().setTitle("TOMFIC API").setDescription("API multiempresa de inventarios y CRM").setVersion("1.0").addBearerAuth().build();
+  SwaggerModule.setup("docs",app,SwaggerModule.createDocument(app,config));
   await app.listen(process.env.PORT||3000);
 }
 
