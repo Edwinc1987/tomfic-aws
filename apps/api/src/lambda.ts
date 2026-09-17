5d54223c413b58a48ed7f5bb772df82082aca4e5
Edwinc1987 <edwincarbonell1987@gmail.com>
2026-09-16 19:04:19 -0500

    Preparar API Nest para AWS Lambda

---
+++ apps/api/src/lambda.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from "@nestjs/core";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import serverlessExpress from "@vendia/serverless-express";
+import { AppModule } from "./app.module";
+
+let proxy:any;
+
+const bootstrap=async()=>{
+  const app=await NestFactory.create(AppModule,new ExpressAdapter());
+  app.enableCors();
+  await app.init();
+  return serverlessExpress({app:app.getHttpAdapter().getInstance()});
+};
+
+export const handler=async(event:any,context:any)=>{
+  proxy=proxy||await bootstrap();
+  return proxy(event,context);
+};
