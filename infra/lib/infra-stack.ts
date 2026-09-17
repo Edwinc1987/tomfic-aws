31794a07eca1b49432dd1895199c5ecb1eed3062
Edwinc1987 <edwincarbonell1987@gmail.com>
2026-09-17 12:16:13 -0500

    Quitar API GW logging (requiere CloudWatch role) + fix CI ankaReport

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -23,10 +23,10 @@ export class InfraStack extends cdk.Stack {
       standardAttributes:{email:{required:true,mutable:true}},
       removalPolicy:cdk.RemovalPolicy.RETAIN,
     });
-    userPool.addGroup(new cognito.CfnUserPoolGroup(this,"OwnerGroup",{userPoolId:userPool.userPoolId,groupName:"OWNER",precedence:0}));
-    userPool.addGroup(new cognito.CfnUserPoolGroup(this,"AdminGroup",{userPoolId:userPool.userPoolId,groupName:"ADMIN",precedence:1}));
-    userPool.addGroup(new cognito.CfnUserPoolGroup(this,"ManagerGroup",{userPoolId:userPool.userPoolId,groupName:"MANAGER",precedence:2}));
-    userPool.addGroup(new cognito.CfnUserPoolGroup(this,"CapturerGroup",{userPoolId:userPool.userPoolId,groupName:"CAPTURER",precedence:3}));
+    userPool.addGroup("OwnerGroup",{groupName:"OWNER",precedence:0});
+    userPool.addGroup("AdminGroup",{groupName:"ADMIN",precedence:1});
+    userPool.addGroup("ManagerGroup",{groupName:"MANAGER",precedence:2});
+    userPool.addGroup("CapturerGroup",{groupName:"CAPTURER",precedence:3});
     const userPoolClient=userPool.addClient("TomficWebClient",{authFlows:{userPassword:true,userSrp:true},oAuth:{scopes:[cognito.OAuthScope.OPENID,cognito.OAuthScope.PROFILE,cognito.OAuthScope.EMAIL]}});
     new cdk.CfnOutput(this,"UserPoolId",{value:userPool.userPoolId});
     new cdk.CfnOutput(this,"UserPoolClientId",{value:userPoolClient.userPoolClientId});
@@ -90,11 +90,10 @@ export class InfraStack extends cdk.Stack {
     importQueue.grantSendMessages(apiLambda);
     if(database){database.secret?.grantRead(apiLambda);}
 
-    const apiLogs=new logs.LogGroup(this,"TomficApiLogs",{logGroupName:"/aws/lambda/tomfic-api",retention:logs.RetentionDays.TWO_WEEKS,removalPolicy:cdk.RemovalPolicy.DESTROY});
     const api=new apigw.LambdaRestApi(this,"TomficApiGateway",{
       handler:apiLambda,
       proxy:true,
-      deployOptions:{stageName:"v1",tracingEnabled:true,loggingLevel:apigw.MethodLoggingLevel.INFO,accessLogDestination:new apigw.LogGroupLogDestination(apiLogs),metricsEnabled:true},
+      deployOptions:{stageName:"v1",tracingEnabled:true,metricsEnabled:true},
       defaultCorsPreflightOptions:{allowOrigins:apigw.Cors.ALL_ORIGINS,allowMethods:apigw.Cors.ALL_METHODS,allowHeaders:["Content-Type","Authorization","x-tenant-id","x-user-role","x-user-id"]},
     });
     new cdk.CfnOutput(this,"ApiUrl",{value:api.url});
